098fb177a1bc9eef0d782994270625c0725f2010
adrycarou896 <adripc@live.com>
2018-12-26 13:49:55 +0100

    Basee

---
+++ src/Caballero.js
@@ -0,0 +1,189 @@
+var derecha = 1;
+var izquierda = 2;
+var arriba = 3;
+var abajo = 4;
+
+var estadoCaminando = 0;
+
+var Caballero = cc.Class.extend({
+    space:null,
+    sprite:null,
+    shape:null,
+    body:null,
+    layer:null,
+    orientacion:derecha,
+    aQuieto:null,
+    aDerecha:null,
+    aIzquierda:null,
+    aArriba:null,
+    aAbajo:null,
+    animacion:null, // Actual
+    estado: estadoCaminando,
+ctor:function (space, posicion, layer) {
+    this.space = space;
+    this.layer = layer;
+
+    // Crear Sprite - Cuerpo y forma
+    this.sprite = new cc.PhysicsSprite("#caballero_quieto_01.png");
+    // Cuerpo dinamico, SI le afectan las fuerzas
+    this.body = new cp.Body(5, Infinity);
+
+    this.body.setPos(posicion);
+    //body.w_limit = 0.02;
+    this.body.setAngle(0);
+    this.sprite.setBody(this.body);
+
+    // Se añade el cuerpo al espacio
+    this.space.addBody(this.body);
+
+    // forma
+    this.shape = new cp.BoxShape(this.body,
+        this.sprite.getContentSize().width,
+        this.sprite.getContentSize().height);
+
+    this.shape.setFriction(1);
+    this.shape.setElasticity(0);
+
+    // forma dinamica
+    this.space.addShape(this.shape);
+
+    // Crear animación - quieto
+    var framesAnimacion = [];
+    for (var i = 1; i <= 2; i++) {
+        var str = "caballero_quieto_0" + i + ".png";
+        var frame = cc.spriteFrameCache.getSpriteFrame(str);
+        framesAnimacion.push(frame);
+    }
+    var animacion = new cc.Animation(framesAnimacion, 0.2);
+    this.aQuieto =
+        new cc.RepeatForever(new cc.Animate(animacion));
+
+    // Crear animación - derecha
+    var framesAnimacion = [];
+    for (var i = 1; i <= 2; i++) {
+        var str = "caballero_derecha_0" + i + ".png";
+        var frame = cc.spriteFrameCache.getSpriteFrame(str);
+        framesAnimacion.push(frame);
+    }
+    var animacion = new cc.Animation(framesAnimacion, 0.2);
+    this.aDerecha =
+        new cc.RepeatForever(new cc.Animate(animacion));
+
+    // Crear animación - izquierda
+    var framesAnimacion = [];
+    for (var i = 1; i <= 2; i++) {
+        var str = "caballero_izquierda_0" + i + ".png";
+        var frame = cc.spriteFrameCache.getSpriteFrame(str);
+        framesAnimacion.push(frame);
+    }
+    var animacion = new cc.Animation(framesAnimacion, 0.2);
+    this.aIzquierda =
+        new cc.RepeatForever(new cc.Animate(animacion));
+
+    // Crear animación - arriba
+    var framesAnimacion = [];
+    for (var i = 1; i <= 2; i++) {
+        var str = "caballero_arriba_0" + i + ".png";
+        var frame = cc.spriteFrameCache.getSpriteFrame(str);
+        framesAnimacion.push(frame);
+    }
+    var animacion = new cc.Animation(framesAnimacion, 0.2);
+    this.aArriba =
+        new cc.RepeatForever(new cc.Animate(animacion));
+
+    // Crear animación - abajo
+    var framesAnimacion = [];
+    for (var i = 1; i <= 2; i++) {
+        var str = "caballero_abajo_0" + i + ".png";
+        var frame = cc.spriteFrameCache.getSpriteFrame(str);
+        framesAnimacion.push(frame);
+    }
+    var animacion = new cc.Animation(framesAnimacion, 0.2);
+    this.aAbajo =
+        new cc.RepeatForever(new cc.Animate(animacion));
+
+    // ejecutar la animación
+    this.animacion = this.aQuieto;
+
+    layer.addChild(this.sprite,10);
+
+},
+actualizar:function(){
+    switch ( this.estado ){
+        case estadoCaminando:
+            if ( this.body.vx > 0.001){
+                if ( this.animacion != this.aDerecha){
+                    this.animacion = this.aDerecha;
+                    this.sprite.stopAllActions();
+                    this.sprite.runAction(this.animacion);
+                }
+            } else if (this.body.vx < -0.001){
+                if ( this.animacion != this.aIzquierda){
+                    this.animacion = this.aIzquierda;
+                    this.sprite.stopAllActions();
+                    this.sprite.runAction(this.animacion);
+                }
+            } else if ( this.body.vy > 0.001){
+                if ( this.animacion != this.aArriba){
+                    this.animacion = this.aArriba;
+                    this.sprite.stopAllActions();
+                    this.sprite.runAction(this.animacion);
+                }
+            } else if (this.body.vy < -0.001){
+                if ( this.animacion != this.aAbajo){
+                    this.animacion = this.aAbajo;
+                    this.sprite.stopAllActions();
+                    this.sprite.runAction(this.animacion);
+                }
+            }
+            else {
+                if ( this.animacion != this.aQuieto){
+                    this.animacion = this.aQuieto;
+                    this.sprite.stopAllActions();
+                    this.sprite.runAction(this.animacion);
+                }
+            }
+        break;
+        /** añadir otros estados, disparar, etc **/
+    }
+},
+moverX:function(movimientoX){
+       if ( movimientoX > 0){
+            if (this.body.vx < 100){
+                this.body.applyImpulse(cp.v(300, 0), cp.v(0, 0));
+            } else { // vx mayor más de 100
+                this.body.vx = 100;
+            }
+        }
+        if ( movimientoX < 0){
+            if (this.body.vx > -100){
+                this.body.applyImpulse(cp.v(-300, 0), cp.v(0, 0));
+            } else { // vx nunca menor que -100
+                this.body.vx = -100; //limitado
+            }
+        }
+        if ( movimientoX == 0 ){
+            this.body.vx = 0;
+        }
+},
+moverY:function(movimientoY){
+        if ( movimientoY > 0){
+             if (this.body.vy < 100){
+                 this.body.applyImpulse(cp.v(0, 30), cp.v(0, 0));
+             } else { // vy mayor más de 100
+                 this.body.vy = 100;
+             }
+        }
+        if ( movimientoY < 0){
+             if (this.body.vy > -100){
+                 this.body.applyImpulse(cp.v(0, -300), cp.v(0, 0));
+             } else { // vy nunca menor que -100
+                 this.body.vy = -100; //limitado
+             }
+        }
+        if ( movimientoY == 0 ){
+             this.body.vy = 0;
+        }
+}
+
+});
---
+++ src/GameScene.js
@@ -0,0 +1,164 @@
+var controles = {};
+var teclas = [];
+
+var GameLayer = cc.Layer.extend({
+    caballero:null,
+    space:null,
+    mapa:null,
+    mapaAncho:0,
+    mapaAlto:0,
+    ctor:function () {
+       this._super();
+       var size = cc.winSize;
+
+       cc.spriteFrameCache.addSpriteFrames(res.caballero_plist);
+
+       // Inicializar Space (sin gravedad)
+       this.space = new cp.Space();
+       /**
+       this.depuracion = new cc.PhysicsDebugNode(this.space);
+       this.addChild(this.depuracion, 10);
+       **/
+
+       this.cargarMapa();
+       this.scheduleUpdate();
+
+       this.caballero = new Caballero(this.space,
+              cc.p(50,150), this);
+
+       cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: this.procesarKeyPressed.bind(this),
+            onKeyReleased: this.procesarKeyReleased.bind(this)
+       }, this);
+
+       return true;
+
+    },
+    update:function (dt) {
+       this.space.step(dt);
+       this.procesarControles();
+       this.caballero.actualizar();
+
+       // Mover cámara
+       var posicionXCamara = this.caballero.body.p.x - this.getContentSize().width/2;
+       var posicionYCamara = this.caballero.body.p.y - this.getContentSize().height/2;
+
+       if ( posicionXCamara < 0 ){
+          posicionXCamara = 0;
+       }
+       if ( posicionXCamara > this.mapaAncho - this.getContentSize().width ){
+          posicionXCamara = this.mapaAncho - this.getContentSize().width;
+       }
+
+       if ( posicionYCamara < 0 ){
+           posicionYCamara = 0;
+       }
+       if ( posicionYCamara > this.mapaAlto - this.getContentSize().height ){
+           posicionYCamara = this.mapaAlto - this.getContentSize().height ;
+       }
+
+       this.setPosition(cc.p( - posicionXCamara , - posicionYCamara));
+
+    },
+    cargarMapa:function () {
+       this.mapa = new cc.TMXTiledMap(res.mapa1_tmx);
+       // Añadirlo a la Layer
+       this.addChild(this.mapa);
+       // Ancho del mapa
+       this.mapaAncho = this.mapa.getContentSize().width;
+       this.mapaAlto = this.mapa.getContentSize().height;
+
+        // Solicitar los objeto dentro de la capa Limites
+        var grupoLimites = this.mapa.getObjectGroup("Limites");
+        var limitesArray = grupoLimites.getObjects();
+
+        // Los objetos de la capa limites
+        // formas estáticas de Chipmunk ( SegmentShape ).
+        for (var i = 0; i < limitesArray.length; i++) {
+              var limite = limitesArray[i];
+              var puntos = limite.polylinePoints;
+              for(var j = 0; j < puntos.length - 1; j++){
+                  var bodyLimite = new cp.StaticBody();
+
+                  var shapeLimite = new cp.SegmentShape(bodyLimite,
+                      cp.v(parseInt(limite.x) + parseInt(puntos[j].x),
+                          parseInt(limite.y) - parseInt(puntos[j].y)),
+                      cp.v(parseInt(limite.x) + parseInt(puntos[j + 1].x),
+                          parseInt(limite.y) - parseInt(puntos[j + 1].y)),
+                      1);
+
+                  shapeLimite.setFriction(1);
+                  shapeLimite.setElasticity(0);
+                  this.space.addStaticShape(shapeLimite);
+              }
+        }
+    },
+    procesarKeyPressed:function(keyCode){
+        console.log("procesarKeyPressed "+keyCode);
+        var posicion = teclas.indexOf(keyCode);
+        if ( posicion == -1 ) {
+            teclas.push(keyCode);
+            switch (keyCode ){
+                case 39:
+                    // ir derecha
+                    console.log("controles.moverX = 1");
+                    controles.moverX = 1;
+                    break;
+                case 37:
+                    // ir izquierda
+                    controles.moverX = -1;
+                    break;
+                case 38:
+                    // ir arriba
+                    controles.moverY = 1;
+                    break;
+                case 40:
+                    // ir abajo
+                    controles.moverY = -1;
+                    break;
+            }
+        }
+    },
+    procesarKeyReleased(keyCode){
+        console.log("procesarKeyReleased "+keyCode);
+        var posicion = teclas.indexOf(keyCode);
+        teclas.splice(posicion, 1);
+        switch (keyCode ){
+            case 39:
+                if ( controles.moverX == 1){
+                    controles.moverX = 0;
+                }
+                break;
+            case 37:
+                if ( controles.moverX == -1){
+                    controles.moverX = 0;
+                }
+                break;
+            case 38:
+                if ( controles.moverY == 1){
+                    controles.moverY = 0;
+                }
+                break;
+            case 40:
+                if ( controles.moverY == -1){
+                    controles.moverY = 0;
+                }
+                break;
+        }
+    },
+    procesarControles:function(){
+        this.caballero.moverX(controles.moverX);
+        this.caballero.moverY(controles.moverY);
+    }
+});
+
+
+
+var GameScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new GameLayer();
+        this.addChild(layer);
+    }
+});
